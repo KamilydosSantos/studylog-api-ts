9e119d26b0e7c2eb3d93c0d611fe10f53843d21d
Kamily <kamily7803@gmail.com>
2024-08-30 11:46:10 -0300

    feat: add route for searching users by name or userName

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import UserRepository from '../repositories/user-repository';
 import UserController from '../controllers/user-controller';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/users', UserController.create);
+usersRouter.get('/users/search', UserController.findByNameOrUserName);
 
 export default usersRouter;
