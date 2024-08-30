e25d13c6424170311a96e7cdd38a7848f87bb03b
Kamily <kamily7803@gmail.com>
2024-08-29 23:57:26 -0300

    feat: add findByNameOrUserName method in user repository

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -6,6 +6,15 @@ class UserRepository extends BaseRepository<User> {
   constructor() {
     super('users');
   }
+
+  findByNameOrUserName(name: string, callback: (user?: User) => void): void {
+    const sql = 'SELECT * FROM users WHERE name LIKE? OR username LIKE? LIMIT 8';
+    const params = [`%${name}%`, `%${name}%`];
+    database.get(sql, params, (_err, row) => {
+      const user = row as User | undefined;
+      callback(user);
+    });
+  }
 }
 
 export default UserRepository;
